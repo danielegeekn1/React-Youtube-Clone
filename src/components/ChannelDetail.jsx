1898c9c5a85b8d13526ec2852f3850edec3b1155
Daniele Basile <daniele.basile.job@gmail.com>
2023-05-12 12:39:30 +0200

    began youtube channel profile component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromApi } from "../utils/fetchFromApi";
 const ChannelDetail = () => {
-  return <div>channel detail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromApi(`search?channelId=${id}&part=snippet%2Cid&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            height: "300px",
+            background:
+              "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
+            zIndex: 10,
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
